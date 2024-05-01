5552dfa356c2bf05fe0c95506183397ea35a8354
a.gazzaev <a.gazzaev@mts.ai>
2024-05-01 18:18:55 +0300

    add tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,11 +1,11 @@
 import requests
 
-res = requests.get("http://host.docker.internal/get_test_prediction")
+res = requests.get("http://host.docker.internal:8000/get_test_prediction")
 res.json().get("result")
 print("--------------------------------")
 print("TEST ENDPOINT RESULT: ",  res)
 
-url = 'http://host.docker.internal/get_real_prediction'
+url = 'http://host.docker.internal:8000/get_real_prediction'
 files = {'media': open('/dataset/PetImages/Cat/3004.jpg', 'rb')}
 res = requests.post(url, files=files)
 res.json().get("result")
